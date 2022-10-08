384f457cf01c3c140262bbb134fc916c80618dff
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-10-07 23:32:14 -0400

    Update maze_runner_modded.cpp

--- maze_runner_modded.cpp
+++ maze_runner_modded.cpp
@@ -6,3 +6,50 @@ Assignment: Project 1 Task D
 I wrote a program that can stream in a maze and set up the array then take in directions to see if they can traverse the custom maze.
 */
 
+
+#include <iostream>
+#include <file.open("maze_and_directions.txt");>
+using namespace std;
+int main()
+
+{
+file.open("maze_and_directions.txt");
+
+int i=-1,j=0,x=0;             
+
+char f[100];                   
+
+cin.getline(f,100); 
+	
+file.open("maze_and_directions.txt");
+
+while(f[x] != '\0')           
+{
+
+if (f[x] =='D')  
+{
+  i++;
+}
+if (f[x] == 'U')                 
+{
+          i--;
+}
+if(f[x] == 'R')                 
+{
+	j++;
+}
+if(f[x] == 'L')                 
+{
+	j--;
+}
+x++;
+}
+
+if (i == 3 && j == 4)         
+cout<<"You got out of the maze.";
+else
+cout<<"You are stuck in the maze.";
+
+return 0;
+
+}
