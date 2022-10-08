8eabce535f4c3f85dc38c0d40e42d6e3bed69199
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-10-07 23:17:54 -0400

    Create maze_runner_modded.cpp

---
+++ maze_runner_modded.cpp
@@ -0,0 +1,8 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Genady Maryash
+Assignment: Project 1 Task D
+I wrote a program that can stream in a maze and set up the array then take in directions to see if they can traverse the custom maze.
+*/
+
